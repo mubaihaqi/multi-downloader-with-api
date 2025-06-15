715d62821f08819c30287a6da2ca04f3234ad7ca
mubaihaqi <muhammadbaihaqidev@gmail.com>
2025-06-15 23:53:34 +0700

    some pesonalisation

---
+++ src/components/navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div
+      className="navbar bg-base-100 shadow-md rounded-b-xl px-4 py-2 mb-2"
+      data-theme="cupcake"
+    >
+      <div className="flex-1">
+        <span className="text-2xl font-extrabold text-info tracking-wide">
+          Multi <span className="text-neutral">Downloader</span>
+        </span>
+      </div>
+      <div className="flex-none">
+        <span className="badge badge-accent badge-md font-semibold">
+          <a href="https://github.com/mubaihaqi">@ mubaihaqi</a>
+        </span>
+      </div>
+    </div>
+  );
+}
