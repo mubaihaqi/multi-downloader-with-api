debac0c0a7ebf4f5b61e15448a299a2cd9ca0d6a
mubaihaqi <muhammadbaihaqidev@gmail.com>
2025-06-28 08:43:18 +0700

    adding seo meta tag

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://multidownloader-orpin.vercel.app",
   vite: {
     plugins: [tailwindcss()],
   },
